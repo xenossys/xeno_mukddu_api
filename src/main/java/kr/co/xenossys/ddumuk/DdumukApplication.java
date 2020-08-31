3b4c4e3e7d31b4eafe2dacdaa9c682174d216d74
xenossys <xenossys@gmail.com>
2020-09-01 01:33:29 +0900

    first commit

---
+++ src/main/java/kr/co/xenossys/ddumuk/DdumukApplication.java
@@ -0,0 +1,13 @@
+package kr.co.xenossys.ddumuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdumukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdumukApplication.class, args);
+	}
+
+}
